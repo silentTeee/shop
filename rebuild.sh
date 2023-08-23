92f88f3d103b232f7e36f989528e6a19b9222670
Jacob Kauffmann <jacob@system76.com>
2023-08-23 12:36:33 -0600

    feat: Pop!_OS patches
    * Add debian directory & rebuild script
    * Set meson options

---
+++ rebuild.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+killall io.elementary.appcenter || true
+VERSION="$(dpkg-parsechangelog -S Version)"
+rm -rf ./obj-x86_64-linux-gnu/
+debuild -b -uc -us -nc
+sudo dpkg -i "../pop-shop_${VERSION}_amd64.deb"
+io.elementary.appcenter
